f6cd98b4ae132d4136bd554f231307c1dc67c898
danilmajorow2355 <allogrant291217@gmail.com>
2023-04-14 15:57:35 +0200

    pr

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import H from './components/H';
+import Ol from './components/Ol';
+import P from './components/P';
+function App() {
+  return (
+    <div className="App">
+      <H />
+      <P />
+      <Ol />
+
+
+    </div>
+  );
+}
+export default App;
---
+++ src/components/H.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function H() {
+    return (
+        <div>      
+            <h1>Заголовок</h1>
+        </div>
+    )
+}
+
+export default H
